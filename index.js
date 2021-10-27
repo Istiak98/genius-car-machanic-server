44caf1968b3d1ecb58b3a95529a3e246e8e262bc
Istiak98 <istiakahmed104127@gmail.com>
2021-10-27 21:42:23 +0600

    added port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
 
